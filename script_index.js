8ce422e8f2f5f99d114bf085080e768598e5838c
Emil270 <83134906+Emil270@users.noreply.github.com>
2022-02-10 15:19:16 +0300

    Update script_index.js

--- script_index.js
+++ script_index.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
-		$.ajax({
-            	url: 'calc.html',
-            	type: 'GET',
-            	beforeSend: function(){
-                	$('body').empty();
-            	},
-            	success: function(responce){        
-                    $('body').append(responce);
-		},
-            	error: function(){
-                	alert('Error!');            
-            	}
+    $("#button_start").click(function(){
+        $.ajax({
+            url: 'calc.html',
+            type: 'GET',
+            beforeSend: function(){
+                $('body').empty();
+            },
+            success: function(responce){        
+                $('body').append(responce);
+            },
+            error: function(){
+                alert('Error!');            
+            }
         });
+    });
 });
